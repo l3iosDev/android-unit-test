827a10cb522e817ee97e5f232306f40d97b749f2
JamesOverlcock <wirayut.temsao@gmail.com>
2021-10-10 18:22:27 +0700

    first commit

---
+++ app/src/androidTest/java/com/l3ios/androidunittest/ResourceComparerTest.kt
@@ -0,0 +1,38 @@
+package com.l3ios.androidunittest
+
+import android.content.Context
+import androidx.test.core.app.ApplicationProvider
+import com.google.common.truth.Truth.assertThat
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+
+class ResourceComparerTest {
+
+    private lateinit var resourceComparer: ResourceComparer
+
+    @Before
+    fun setup() {
+        resourceComparer = ResourceComparer()
+    }
+
+    @After
+    fun teardown() {
+
+    }
+
+    @Test
+    fun stringResourceSameAsGivenString_returnsTrue() {
+        val context = ApplicationProvider.getApplicationContext<Context>()
+        val result = resourceComparer.isEqual(context, R.string.app_name, "AndroidUnitTest")
+        assertThat(result).isTrue()
+    }
+
+    @Test
+    fun stringResourceDifferenceAsGivenString_returnsFalse() {
+        val context = ApplicationProvider.getApplicationContext<Context>()
+        val result = resourceComparer.isEqual(context, R.string.app_name, "Hello")
+        assertThat(result).isFalse()
+    }
+
+}
---
+++ app/src/main/java/com/l3ios/androidunittest/ResourceComparer.kt
@@ -0,0 +1,11 @@
+package com.l3ios.androidunittest
+
+import android.content.Context
+
+class ResourceComparer {
+
+    fun isEqual(context: Context, resId: Int, string: String): Boolean {
+        return context.getString(resId) == string
+    }
+
+}
